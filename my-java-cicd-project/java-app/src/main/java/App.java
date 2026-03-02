1ad3a9fea4d4db0795efdf88005a0faf61462b5b
prasanna <prasannayendluri45@gmail.com>
2026-03-02 12:37:43 -0600

    Initial commit - Java CI/CD Project

---
+++ my-java-cicd-project/java-app/src/main/java/App.java
@@ -0,0 +1,49 @@
+public class App {
+
+    public static void main(String[] args) {
+
+        String content = """
+Integrated SonarQube for automated static code analysis and quality gate enforcement.
+
+Containerized the application using Docker and pushed versioned images to a container registry.
+
+Automated Kubernetes deployments using Argo CD following GitOps principles. Hosted application on AWS EC2 and configured secure SSH-based server access.
+
+Developed shell scripts to update Kubernetes manifest repositories, eliminating manual release efforts automatically.
+
+Reduced deployment time and manual intervention by fully automating.
+
+CERTIFICATES
+
+Continuous Integration and Delivery (DevOps) | Infosys - Dec 2024
+Developed practical knowledge of CI/CD pipelines, automation, version control, and deployment practices to streamline software delivery.
+
+Java Programming | Cisco Networking Academy - Jun 2025
+Built strong foundations in Java, focusing on OOP, multithreading, and scalable application development.
+
+Web Development | Prodigy Info Tech - Dec 2024
+Developed responsive web applications with front-end design and back-end integration.
+
+SOFT SKILLS
+
+Problem-solving and analytical thinking
+Effective communication and collaboration
+Adaptability to new tools and technologies
+Time management
+
+B.Tech: IT, Expected in 04/2026
+MLR Institute of Technology - Hyderabad
+GPA: 7.5 CGPA
+
+Intermediate: MPC, 01/2022
+Narayana JR. College - Guntur
+GPA: 83%
+
+High School: SSC, 01/2020
+Holy Faith (E.M) High School - Addanki
+GPA: 10 CGPA
+""";
+
+        System.out.println(content);
+    }
+}
